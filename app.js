2a56c7f707b8ad7b530faeaf8cbb5f73e9b73070
Michae Ion <michael.ion.mi@outlook.com>
2022-08-26 01:01:33 -0400

    created games list and create db collection functionn

--- app.js
+++ app.js
@@ -5,6 +5,81 @@ const db = 'game'
 const collection = 'reviews'
 const client = new MongoClient(uri)
 
+let games = [
+    {
+        name: 'Cyberpunk 2077',
+        rating: 9.5,
+        reviews:[
+            {
+                username: "Gamer_Mike",
+                review: "Best open world game out there!"
+            },
+            {
+                username: 'NPC_Charlie',
+                review: "Fun and addictive thrill ride"
+            },
+            {
+                username: "Accolade_Empire",
+                review: 'Non-stop action, fun and excitement'
+            }
+                ]
+            },
+    {
+        name: 'Dying Light 2',
+        rating: 8.3,
+        reviews:[
+            {
+                username: "Gamer_Mike",
+                review: "Scary AF"
+            },
+            {
+                username: 'NPC_Charlie',
+                review: "Staying human is hard"
+            },
+            {
+                username: "Accolade_Empire",
+                review: 'Zombie bashing fun'
+            }
+        ]
+    },
+    {
+        name: 'Way of The Hunter',
+        rating: 9.5,
+        reviews:[
+            {
+                username: "Gamer_Mike",
+                review: "50 Miles of open world hunting fun"
+            },
+            {
+                username: 'NPC_Charlie',
+                review: "Realistic hunting game, "
+            },
+            {
+                username: "Accolade_Empire",
+                review: 'Non-stop action, fun and excitement'
+            }
+        ]
+    },
+    {
+        name: 'Ghostwire Tokyo',
+        rating: 8.2,
+        reviews:[
+            {
+                username: "Gamer_Mike",
+                review: "Update pending check back soon!"
+            },
+            {
+                username: 'NPC_Charlie',
+                review: "Visualistically Stunning"
+            },
+            {
+                username: "Accolade_Empire",
+                review: 'Still new to this one, check back soon!'
+            }
+                ]
+            }
+]
+            
 const main = async () => {
     try {
         await client.connect()
@@ -18,3 +93,10 @@ const main = async () => {
 
 main().catch(console.error)
 
+
+const createListing = async (client, document) => {
+    const result = await client 
+    .db(db)
+    .collection(collection)
+    .insertMany(document)
+}
